44523eaf58000c16776a5662277deb98cd6359f7
abhisheksaxena15 <saxenaabhishek9009@gmail.com>
2026-01-16 21:52:10 +0530

    app.js error resolved

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from '   express'
 import  cors from 'cors'
 import cookieParser from 'cookie-parser';
 
@@ -14,14 +14,16 @@ app.use(cors(
 
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({
   extended: true,
   limit: "16kb"
 }));
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+// routes imports
+import userRouter from './routes/user.routes.js';
+// routes declaration
+app.use('/api/v1/users', userRouter);
 
 export { app };
